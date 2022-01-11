171420d24c027b2c3c9dc92669d887422a9315bd
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-11 13:46:27 +1030

    wrote program functionality

---
+++ GST-calculator.c
@@ -0,0 +1,15 @@
+/* takes price and returns total with 10% GST tax added */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int price, tax;
+
+    printf("Enter an amount: \n");
+    scanf("%d", &price);
+
+    tax = price / 10.0;
+
+    printf("With GST added: %d", price + tax);
+}
